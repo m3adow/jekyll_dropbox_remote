bee005b2a62c710ec1947b912fbd61f119823be5
m3adow_boerse <mail-lps@gewim.ml>
2015-08-04 16:28:38 +0200

    * Still trying to get deploy_to_gh_pages to work...at all costs

--- jekyll_dropbox_remote.py
+++ jekyll_dropbox_remote.py
@@ -126,7 +126,9 @@ def jekyll_build(logger, **kwargs):
 
 def deploy_to_gh_pages(logger, **kwargs):
     if 'jekyll_base_dir' not in kwargs:
-        logger.error("Couldn't find jekyll_base_dir in config. Skipping taks %s" % kwargs['task_name'])
+        logger.error("Couldn't find jekyll_base_dir in config (%s). Skipping taks %s" % (
+            kwargs, kwargs['task_name'])
+            )
         return
 
     try:
